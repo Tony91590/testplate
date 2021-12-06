e05e27b602efbe93c2688987b078b6cd055441fe
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2021-12-06 12:12:56 +0100

    Update repack-squashfs-mi.sh

--- repack-squashfs-mi.sh
+++ repack-squashfs-mi.sh
@@ -89,6 +89,7 @@ NVRAM
 
 # modify root password
 sed -i "s@root:[^:]*@root:${ROOTPW}@" "$FSDIR/etc/shadow"
+chmod 0555      "$FSDIR/etc/shadow"
 
 # stop phone-home in web UI
 cat <<JS >> "$FSDIR/www/js/miwifi-monitor.js"
