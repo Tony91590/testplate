deed2607f134ef07d2485b107b088bd1fd752a23
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2021-12-06 10:59:17 +0100

    Update repack-squashfs-mi.sh

--- repack-squashfs-mi.sh
+++ repack-squashfs-mi.sh
@@ -39,6 +39,21 @@ cat ./language-packages/languages.txt >>$FSDIR/usr/lib/opkg/status
 chmod 755 $FSDIR/usr/lib/opkg/info/luci-i18n-*.prerm
 chmod 755 $FSDIR/etc/uci-defaults/luci-i18n-*
 
+# replace www from global
+cp -rf www/* "$FSDIR/www/"
+
+# copy the latest firmware of wifi
+cp -R etc/* "$FSDIR/etc/"
+
+# copy the latest firmware of wifi
+cp -R lib/* "$FSDIR/lib/"
+
+# replace luci from international firmware
+cp -R lua/* "$FSDIR/usr/lib/lua/"
+
+# replace luci from international firmware
+# cp -R xiaoqiang/* "$FSDIR/usr/share/xiaoqiang/"
+
 # modify dropbear init
 sed -i 's/channel=.*/channel=release2/' "$FSDIR/etc/init.d/dropbear"
 sed -i 's/flg_ssh=.*/flg_ssh=1/' "$FSDIR/etc/init.d/dropbear"
@@ -71,6 +86,11 @@ NVRAM
 # modify root password
 sed -i "s@root:[^:]*@root:${ROOTPW}@" "$FSDIR/etc/shadow"
 
+# stop phone-home in web UI
+cat <<JS >> "$FSDIR/www/js/miwifi-monitor.js"
+(function(){ if (typeof window.MIWIFI_MONITOR !== "undefined") window.MIWIFI_MONITOR.log = function(a,b) {}; })();
+JS
+
 # add xqflash tool into firmware for easy upgrades
 cp xqflash "$FSDIR/sbin"
 chmod 0755      "$FSDIR/sbin/xqflash"
@@ -95,27 +115,6 @@ sed -i '/start_service(/a return 0' $FSDIR/etc/init.d/messagingagent.sh
 for f in $FSDIR/etc/crontabs/*; do
 	sed -i 's/^/#/' $f
 done
-
-# replace www from global
-cp -rf www/* "$FSDIR/www/"
-
-# copy the latest firmware of wifi
-cp -R etc/* "$FSDIR/etc/"
-
-# copy the latest firmware of wifi
-cp -R lib/* "$FSDIR/lib/"
-
-# replace luci from international firmware
-cp -R lua/* "$FSDIR/usr/lib/lua/"
-
-# replace luci from international firmware
-# cp -R xiaoqiang/* "$FSDIR/usr/share/xiaoqiang/"
-
-# mark web footer so that users can confirm the right version has been flashed
-sed -i 's/romVersion%>/& xqrepack/;' "$FSDIR/usr/lib/lua/luci/view/web/inc/footer.htm"
-
-# as a last-ditch effort, change the *.miwifi.com hostnames to localhost
-sed -i 's@\w\+.miwifi.com@localhost@g' $FSDIR/etc/config/miwifi
 
 # stop phone-home in web UI
 cat <<JS >> "$FSDIR/www/js/miwifi-monitor.js"
